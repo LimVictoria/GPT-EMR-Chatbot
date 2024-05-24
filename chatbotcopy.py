3b3c3cf4da6fc3721dba57aca1e8ef3f5acd8b70
LimVictoria <147840820+LimVictoria@users.noreply.github.com>
2024-05-24 10:32:19 +0800

    Update chatbotcopy.py

--- chatbotcopy.py
+++ chatbotcopy.py
@@ -11,7 +11,7 @@ def generate_response(prompt, temperature):
     try:
         completions = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-1106",  
-            messages=[{'role': 'system', 'content': "You are a well-rounded doctor that answer questions with high accuracy." },
+            messages=[{'role': 'system', 'content': "You are a doctor treating your patient" },
                       {'role': 'user', 'content': prompt}
                      ],
             temperature=temperature
